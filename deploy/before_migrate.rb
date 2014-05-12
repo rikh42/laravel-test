de5edccaaba003b03b6fb3621269da81ff20d1c1
Rik Heywood <rik@smallneatbox.co.uk>
2014-05-12 20:00:51 +0100

    Updated scripts with debug info

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,8 +1,12 @@
 
-#current_release = release_path
+Chef::Log.info("=== Hook: before_migrate for #{release_path}")
+
+shared_path = ::File.expand_path("#{release_path}/../../shared")
+Chef::Log.info("= Shared path is #{shared_path}")
 
 
 # Run 'composer install'
+Chef::Log.info("= Running Composer to install dependencies...")
 execute "composer install" do
 	cwd release_path
 	command "composer install --no-dev --no-interaction"
--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -2,7 +2,15 @@
 # Run DB migrations etc here...
 
 
+
+Chef::Log.info("=== Hook: before_symlink for #{release_path}")
+
+shared_path = ::File.expand_path("#{release_path}/../../shared")
+Chef::Log.info("= Shared path is #{shared_path}")
+
+
 # Run 'composer install'
+Chef::Log.info("= Running artisan DB migration")
 execute "composer install" do
 	cwd release_path
 	command "php artisan migrate --env=production"
