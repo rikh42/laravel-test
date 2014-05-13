2c26ddf4a27bfadeeafcabf404e5ecda9651ba1b
Rik Heywood <rik@smallneatbox.co.uk>
2014-05-13 18:34:53 +0100

    Added a useful block for changing permissions

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -21,6 +21,15 @@ execute "fixup storage folder" do
   command "chmod -R 777 #{release_path}/app/storage"
 end
 
+
+# A method to changing owner of paths and permissions
+#execute "chown app cache and log dirs to deploy:www-data" do
+#    user "root"
+#    cwd release_path
+#    command "mkdir -p app/cache; chown -R deploy:www-data app/cache; chown -R deploy:www-data app/logs; find app/cache -type d | xargs -r chmod 0770; find app/cache -type f | xargs -r chmod 0660; find app/logs -type d | xargs -r chmod 0770; find app/logs -type f | xargs -r chmod 0660;"
+#    action :run
+#end
+
 # Run Database Migrations
 Chef::Log.info("= Running artisan DB migration")
 execute "artisan migrate" do
