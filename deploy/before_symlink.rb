dea5bdbb63a99ed8840226740a3d0248edb5d72d
Rik Heywood <rik@smallneatbox.co.uk>
2014-05-13 08:03:19 +0100

    Attempt to set up the DB config for the app

--- deploy/before_symlink.rb
+++ deploy/before_symlink.rb
@@ -1,19 +1,33 @@
 
-# Run DB migrations etc here...
-
-
-
-Chef::Log.info("=== Hook: before_symlink for #{release_path}")
+# Called before the symlink is switched over
 
 shared_path = ::File.expand_path("#{release_path}/../../shared")
+
+Chef::Log.info("=== Hook: before_symlink for #{release_path}")
 Chef::Log.info("= Shared path is #{shared_path}")
 
 
-# Run 'composer install'
+# Set up the DB config so the app can access it
+Chef::Log.info("= Creating link to Database config file")
+execute "OpsWorks.php Link setup" do
+	cwd release_path
+	command "ln -s #{release_path}/../../shared/opsworks.php #{release_path}/opsworks.php"
+	action :run
+end
+
+
+# Make some folders writable
+
+
+# Run Database Migrations
 Chef::Log.info("= Running artisan DB migration")
-execute "composer install" do
+execute "artisan migrate" do
 	cwd release_path
 	command "php artisan migrate --env=production"
 	action :run
 end
+
+
+
+# clear memcached etc
 
