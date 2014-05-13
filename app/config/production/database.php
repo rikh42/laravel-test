d1da1871d3536d5183772dda3fe2daf289c1f9b0
Rik Heywood <rik.heywood@pod-point.com>
2014-05-13 13:33:19 +0100

    Added production database

---
+++ app/config/production/database.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+
+	'connections' => array(
+
+		'mysql' => array(
+			'driver'    => 'mysql',
+			'host'      => 'localhost',
+			'database'  => 'laravel',
+			'username'  => 'root',
+			'password'  => 'QaxRazQPRoadbzQSylqh',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+		),
+
+	),
+
+);
